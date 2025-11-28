b2899229c8c49d8db93547967ba59b6de521b911
AlexInABox <hobby@alexinabox.de>
2025-11-28 19:56:41 +0100

    fix: resolve backendurl safer

--- frontend/constants/api.ts
+++ frontend/constants/api.ts
@@ -1,13 +1,20 @@
+import { Platform } from "react-native";
+
 export const getBackendURL = () => {
-  if (window.location.origin.includes('localhost:')) {
-    return origin.replace(':8081', ':8787') + '/api/';
+  if (Platform.OS === 'web') {
+    if (window.location.origin.includes('localhost:')) {
+      return window.location.origin.replace(':8081', ':8787') + '/api/';
+    }
+    const apiUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
+    if (apiUrl) {
+      return apiUrl;
+    }
+    return 'https://dev.splitsmart.de/api/';
+  } else {
+    const apiUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
+    if (apiUrl) {
+      return apiUrl;
+    }
+    return 'https://dev.splitsmart.de/api/';
   }
-
-  const apiUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
-  if (apiUrl) {
-    return apiUrl;
-  }
-
-  // Fallback
-  return 'https://dev.splitsmart.de/api/';
-};
+}
